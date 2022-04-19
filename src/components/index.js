cbfe46f3cc29719d4ff7a422baaae6fff8542a5f
Fash Muyhee <fashmuyidavid@gmail.com>
2022-04-19 18:34:39 +0100

    deleted

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,8 @@
 import ScrollContainer from './ScrollContainer';
-import Container from './Container';
 import List from './List';
 import Button from './Button';
 import TextInput from './TextInput';
 import Input from './Input';
 
 
-export { ScrollContainer, Container, List, Button, TextInput, Input };
+export { ScrollContainer, List, Button, TextInput, Input };
